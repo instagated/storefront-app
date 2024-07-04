7fe331d8aa9258f5b60de8287d412b20b65054d4
INSTAGATOR <naiseips@pm.me>
2024-07-03 21:25:15 -0400

    ii

---
+++ app.config.js
@@ -0,0 +1,12 @@
+export default {
+  android: {
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+  },
+  expo: {
+    extra: {
+      eas: {
+        projectId: "f4a5b0bc-36e1-4d33-8ea3-100158083f9a"
+      }
+    }
+  }
+};
